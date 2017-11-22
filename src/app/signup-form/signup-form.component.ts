9c39324092c810d29e0053a3c92771e866f36636
Paul <test@mail.ru>
2017-11-22 21:21:33 +0400

    less 08-103

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,18 +9,26 @@ import {UsernameValidators} from "./username.validator";
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      UsernameValidators.shouldBeUnique,
-      Validators.minLength(3),
-      Validators.maxLength(15),
-      Validators.pattern('^[_a-zA-Z0-9а-яА-Я ]+$'),
-      UsernameValidators.cannotContainSpace
-    ]),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        UsernameValidators.shouldBeUnique,
+        Validators.minLength(3),
+        Validators.maxLength(15),
+        Validators.pattern('^[_a-zA-Z0-9а-яА-Я ]+$'),
+        UsernameValidators.cannotContainSpace
+      ]),
+      password: new FormControl('', Validators.required)
+    })
   });
 
+  login() {
+    this.form.setErrors({
+      invalidLogin: true
+    });
+  }
+
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
